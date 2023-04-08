755c8122aabdb3e66502202cce53a3cd5c599d91
Umama Masroor <umamafatima319@gmail.com>
2023-04-07 17:12:33 -0700

    question number 3

--- add.js
+++ add.js
@@ -25,4 +25,25 @@
 // }
 
 // question # 3.............
+let num1=prompt("Enter the frst number:");
+let num2=prompt("Enter the second number:");
+let num3=prompt("Enter the third number:");
+let num4=prompt("Enter the fourth number:");
+let num5=prompt("Enter the fifth number:");
+
+let max = num1;
+if(num2>max){
+    max = num2;
+}
+if(num3>max){
+    max = num3;
+}
+if(num4>max){
+    max = num4;
+}
+if(num5>max){
+    max = num5;
+}
+console.log("The largest number is " + max);
+
 
